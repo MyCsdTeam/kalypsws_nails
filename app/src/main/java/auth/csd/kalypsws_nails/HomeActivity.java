e3dc909c363406f97e8f4f9e367abbebc43a5381
AlexTsimerikas <alex.tsime@gmail.com>
2026-05-08 13:23:10 +0300

    activity_home.xml changes

--- app/src/main/java/auth/csd/kalypsws_nails/HomeActivity.java
+++ app/src/main/java/auth/csd/kalypsws_nails/HomeActivity.java
@@ -1,13 +1,16 @@
 package auth.csd.kalypsws_nails; // ΠΡΟΣΟΧΗ: Κράτα το δικό σου package name εδώ
 
 import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Αυτή η γραμμή συνδέει την Java με το XML παρουσιαστικό που έφτιαξες
+        EdgeToEdge.enable(this);
+        // Αυτή η γραμμή συνδέει τη Java με το XML παρουσιαστικό που έφτιαξες
         setContentView(R.layout.activity_home);
     }
 }
